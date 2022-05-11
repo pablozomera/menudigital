ac2682a5e366e7a5a23d9229c15f7143d97ea5f5
pablozomera <80721561+pablozomera@users.noreply.github.com>
2022-05-11 14:36:10 -0500

    Delete mresponsive.js

--- js/mresponsive.js
+++
@@ -1,5 +0,0 @@
-const hamburgerMenu = document.querySelector('.hamburger');
-const menuIsActive = () => {
-    hamburgerMenu.classList.toggle('active');
-};
-hamburgerMenu.addEventListener('click', menuIsActive)
